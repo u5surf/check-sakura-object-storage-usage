ee1175b5ae28733be84794935a91c60e4ebd4624
u5surf <u5.horie@gmail.com>
2025-12-22 21:22:16 +0900

    Change official environment variables

--- lib/check-usage_test.go
+++ lib/check-usage_test.go
@@ -105,8 +105,8 @@ func TestParseOption(t *testing.T) {
 				opts.Critical = nil
 				opts.Site = nil
 				opts.Bucket = nil
-				t.Setenv("SAKURA_API_ACCESS_TOKEN", "SAKURA_API_ACCESS_TOKEN")
-				t.Setenv("SAKURA_API_ACCESS_TOKEN_SECRET", "SAKURA_API_ACCESS_TOKEN_SECRET")
+				t.Setenv("SAKURACLOUD_ACCESS_TOKEN", "SAKURACLOUD_ACCESS_TOKEN")
+				t.Setenv("SAKURACLOUD_ACCESS_TOKEN_SECRET", "SAKURACLOUD_ACCESS_TOKEN_SECRET")
 			},
 			args: []string{
 				"-b",
@@ -121,8 +121,8 @@ func TestParseOption(t *testing.T) {
 				opts.Critical = nil
 				opts.Site = nil
 				opts.Bucket = nil
-				t.Setenv("SAKURA_API_ACCESS_TOKEN", "SAKURA_API_ACCESS_TOKEN")
-				t.Setenv("SAKURA_API_ACCESS_TOKEN_SECRET", "SAKURA_API_ACCESS_TOKEN_SECRET")
+				t.Setenv("SAKURACLOUD_ACCESS_TOKEN", "SAKURACLOUD_ACCESS_TOKEN")
+				t.Setenv("SAKURACLOUD_ACCESS_TOKEN_SECRET", "SAKURACLOUD_ACCESS_TOKEN_SECRET")
 			},
 			args: []string{
 				"-s",
